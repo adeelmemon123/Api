918031b8deafd88d93a7d78225c50025c8669cc2
adeelmemon123 <adeel.memon03000@gmail.com>
2024-02-02 16:25:48 +0500

    hello

--- main.js
+++ main.js
@@ -1,60 +1,15 @@
 const express = require('express');
-const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 9000;
 
 app.use(cors());
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'squareone',
-  
-  
-  
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error('Unable to connect to MySQL:', err);
-  } else {
-    console.log('Connected to MySQL');
-  }
-});
-
-app.post('/addmember', (req, res) => {
-  const requestData = req.body;
-
-  const sql =
-    'INSERT INTO addmember (membername, password, email, role, permission, profilepicture, roledescription) VALUES (?, ?, ?, ?, ?, ?, ?)';
-  db.query(
-    sql,
-    [
-      requestData.membername,
-      requestData.password,
-      requestData.email,
-      requestData.role,
-      requestData.permission,
-      requestData.profilepicture,
-      requestData.roledescription,
-    ],
-    (err, result) => {
-      if (err) {
-        console.error('Error inserting data:', err);
-        return res.status(500).json({ error: 'Internal Server Error', message: err.message });
-      } else {
-        console.log('Data inserted successfully');
-        return res.status(200).json({ message: 'Data inserted successfully' });
-      }
-    }
-  );
-});
+// Define your routes or other middleware here
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
